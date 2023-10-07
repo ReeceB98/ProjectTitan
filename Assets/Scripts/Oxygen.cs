d9fe0b424fc5f9e6019813cc8ec984b1f2a8ad23
Liam <liam2.symes@live.uwe.ac.uk>
2023-10-07 15:52:50 +0100

    Oxygen Bar and depletion working

--- Assets/Scripts/Oxygen.cs
+++ Assets/Scripts/Oxygen.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Oxygen : MonoBehaviour
 {
     public float maxOxygen = 100; //Starting oxygen level
     public float currentOxygen;
     public float oxygenDepletionRate = 1; //Rate of depletion of oxygen
+    public Image oxygenBar;
+    public Text oxygenText;
 
     void Start()
     {
@@ -34,9 +36,24 @@ public class Oxygen : MonoBehaviour
         }
     }
 
+    void OxygenBarFiller()
+    {
+        oxygenBar.fillAmount = currentOxygen / maxOxygen;
+    }
+
+    void ColorChanger()
+    {
+        Color oxygenColor = Color.Lerp(Color.red, Color.green, (currentOxygen / maxOxygen));
+
+        oxygenBar.color = oxygenColor;
+    }
 
     void Update()
     {
+        oxygenText.text = "Oxygen";
+        OxygenBarFiller();
+        ColorChanger();
+
         //Check if there is any oxygen remaining
         if (currentOxygen > 0)
         {
